619e49627a5267d3faaf69a0a7bc72873a882741
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-17 12:43:22 +0200

    add google api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// google api key
+const API_KEY = 'AIzaSyDxJ0R_tp-bdJND_2HBT8FOkli43r980Z0';
+
 // Create new component. This should produce HTML.
 const App = () => {
     return <div>Hello!</div>;
