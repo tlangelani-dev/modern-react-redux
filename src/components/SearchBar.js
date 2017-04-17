9af6a668dd653dcbcac7be244f19dcb414c61307
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-17 13:03:52 +0200

    add events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
+class SearchBar extends Component {
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    render() {
+        return <input type="text" onChange={this.onInputChange} placeholder="Enter youtube video to search..." />;
+    }
 };
 
 export default SearchBar;
